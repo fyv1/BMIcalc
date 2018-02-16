929cf59980863aba1e37a39296f32bddf63f6910
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2018-02-16 19:08:31 +0100

    Computing Values

--- src/pl/fyv/bmicalc/model/BmiCalc.java
+++ src/pl/fyv/bmicalc/model/BmiCalc.java
@@ -1,38 +1,63 @@
 package pl.fyv.bmicalc.model;
 
 import javafx.beans.binding.DoubleBinding;
-import javafx.beans.binding.IntegerBinding;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.DoubleProperty;
+
+
 
 
 public class BmiCalc {
 
-    private final IntegerProperty height;
-    private final IntegerProperty mass;
 
-    private final StringProperty output = null;
+    public double getHeight() {
+        return height.get();
+    }
 
-    public BmiCalc(IntegerProperty height, IntegerProperty mass) {
+    public DoubleProperty heightProperty() {
+        return height;
+    }
 
+    public void setHeight(double height) {
+        this.height.set(height);
+    }
+
+    private final DoubleProperty height;
+
+    public double getMass() {
+        return mass.get();
+    }
+
+    public DoubleProperty massProperty() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass.set(mass);
+    }
+
+    private final DoubleProperty mass;
+
+    public BmiCalc(DoubleProperty height, DoubleProperty mass) {
         this.height = height;
         this.mass = mass;
     }
 
-    IntegerBinding db = new IntegerBinding() {
+
+    DoubleBinding db = new DoubleBinding() {
         @Override
-        protected int computeValue() {
-            return mass.get()/(height.get()*height.get()); // wzór na bmi
+        protected double computeValue() {
+            return getMass()/((getHeight())*getHeight()); // wzór na bmi
         }
     };
 
-    public void interprete(IntegerBinding score) {
-        int db = score.get();
-        if(12 < db && db < 18) output.set("Niedowaga");
-        if(18 < db && db < 25) output.set("Norma");
-        if(25 < db && db < 30) output.set("Nadwaga");
-        if(30 < db && db < 40) output.set("Otyłość");
-        if(40 < db && db < 43) output.set("Poważna otyłość");
+    public String interprete() {
+        double score = db.get();
+        if(12 < score && score < 18) return "Niedowaga";
+        if(18 < score && score < 25) return "Norma";
+        if(25 < score && score < 30) return "Nadwaga";
+        if(30 < score && score < 40) return "Otyłość";
+        if(40 < score && score < 43) return "Poważna otyłość";
+        return "Nieznana wartość";
     }
 }
 
